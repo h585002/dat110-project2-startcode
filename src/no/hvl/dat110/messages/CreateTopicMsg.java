5e068070afc24938adbb2fdaefa7843b687b220d
rolfe <rolfe@LAPTOP-UP59N4Q7>
2021-03-03 13:51:33 +0100

    createmsg og deletemsg ferdig

--- src/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -4,9 +4,27 @@ public class CreateTopicMsg extends Message {
 	
 	// message sent from client to create topic on the broker
 	
+
 	// TODO: 
 	// Implement object variables - a topic is required
+	String topic;
 	
-	// Constructor, get/set-methods, and toString method
-    // as described in the project text	
+	// Constructor
+	public CreateTopicMsg(String user, String topic) {
+		super(MessageType.CONNECT, user);
+		this.topic = topic;
+	}
+    
+	//get/set-methods
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+	//toString method
+	public String toString() {
+		return "Topic: " + topic + "   "  + super.toString();
+	} 
 }
